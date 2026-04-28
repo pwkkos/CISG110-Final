e55b71c33260772817f69dcda4da720bc34a36e7
pwkkos <jasmineluc90@gmail.com>
2026-04-27 20:42:26 -0700

    teddy explodes

--- cisg-110-final/scripts/player.gd
+++ cisg-110-final/scripts/player.gd
@@ -77,7 +77,13 @@ func _on_kick_right_body_shape_entered(body_rid: RID, body: Node2D, body_shape_i
 	print(body.name)
 	body.apply_central_impulse(_rightKickDir)
 
+	if body is Prop:
+		body._was_kicked = true
+
 func _on_kickleft_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	print("kick left")
 	print(body.name)
 	body.apply_central_impulse(_leftKickDir)
+
+	if body is Prop:
+		body._was_kicked = true
---
+++ cisg-110-final/scripts/props.gd
@@ -0,0 +1,18 @@
+class_name Prop
+extends RigidBody2D
+
+@export var _explosion_VFX: CPUParticles2D
+
+var _was_kicked: bool = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	max_contacts_reported = 3
+	contact_monitor = true
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	if _was_kicked:
+		print("prop exploded")
+		_explosion_VFX.emitting = true
+		_explosion_VFX.reparent(get_parent())
+		queue_free()
